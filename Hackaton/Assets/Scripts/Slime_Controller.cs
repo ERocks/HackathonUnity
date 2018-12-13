a7b2c4b239becfe3552a432ef9a226928fdfa900
Héctor Polaino <hectorpolaino@gmail.com>
2018-12-13 14:50:53 +0100

    Comportamientos Finales Slime

--- Hackaton/Assets/Scripts/Slime_Controller.cs
+++ Hackaton/Assets/Scripts/Slime_Controller.cs
@@ -9,7 +9,7 @@ public class Slime_Controller : MonoBehaviour, IDamageable, IMovable
     public float jumpForceX = 400;
     public float clickForceY = 300;
     public float clickForceX = 300;
-    public float shootForce = 200;
+    //public float shootForce = 200;
     public float boomerangThreshold = 1f;
     public float jumpOverThreshold = 1.5f;
     public bool grounded;
@@ -17,18 +17,19 @@ public class Slime_Controller : MonoBehaviour, IDamageable, IMovable
     public int secondsToJump = 3;
     public int secondsToShoot = 0;
     public int m_EnemyDamage = 50;
-    public GameObject bulet;
+    //public GameObject bulet;
     public LayerMask groundLayer;
     public Transform groundCheck;
-    public Transform shooter;
-    public Transform nodo;
-    public enum EnemyType { jumping, stand, fire };
+    //public Transform shooter;
+    //public Transform nodo;
+    public enum EnemyType { jumping, stand, immovable
+    };
     public EnemyType type;
     public Material[] slimeMaterials = new Material[3];
     Rigidbody2D body;
     Vector3 overCircle;
-    float timeToJump, timeToShoot;
-    int secondsJump, secondsShoot;
+    float timeToJump;// timeToShoot;
+    int secondsJump;// secondsShoot;
 
     // Use this for initialization
     void Start()
@@ -40,7 +41,7 @@ public class Slime_Controller : MonoBehaviour, IDamageable, IMovable
     {
         body = this.GetComponent<Rigidbody2D>();
         timeToJump = 0f;
-        this.transform.position = nodo.position;
+        //this.transform.position = nodo.position;
         if (type == EnemyType.jumping)
         {
             this.GetComponent<Renderer>().material = slimeMaterials[0];
@@ -49,9 +50,10 @@ public class Slime_Controller : MonoBehaviour, IDamageable, IMovable
             jumpForceX = 0;
             this.GetComponent<Renderer>().material = slimeMaterials[1];
         }
-        else if (type == EnemyType.fire)
+        else if (type == EnemyType.immovable)
         {
-            jumpForceX = 0;
+            jumpForceY = 0;
+            jumpForceX = 7;
             this.GetComponent<Renderer>().material = slimeMaterials[2];
         }
     }
@@ -77,14 +79,14 @@ public class Slime_Controller : MonoBehaviour, IDamageable, IMovable
     void Update()
     {
         timeToJump += Time.deltaTime;
-        timeToShoot += Time.deltaTime;
+        //timeToShoot += Time.deltaTime;
         secondsJump = (int)timeToJump % 60;
-        secondsShoot = (int)timeToShoot % 60;
+        //secondsShoot = (int)timeToShoot % 60;
         overCircle = this.transform.position;
         overCircle.x += (jumpForceX / 25) * direction;
         overCircle.y -= 0.5f;
         //Debug.Log("Overlap" + Physics2D.OverlapCircle(overCircle, 0.5f).tag);
-        if (secondsJump >= secondsToJump && grounded && type == EnemyType.jumping && secondsToJump > 1)
+        if (secondsJump >= secondsToJump && grounded && type == EnemyType.jumping && secondsToJump > 0)
         {
             if (Physics2D.OverlapCircleAll(overCircle, 0.5f, groundLayer).Length > 0)
             {
@@ -97,11 +99,26 @@ public class Slime_Controller : MonoBehaviour, IDamageable, IMovable
             }
             timeToJump = 0;
         }
+        overCircle = this.transform.position;
+        overCircle.x += 1 * direction;
+        overCircle.y -= 0.5f;
+        if (type == EnemyType.immovable)
+        {
+            if (Physics2D.OverlapCircleAll(overCircle, 0.5f, groundLayer).Length > 0)
+            {
+                jump();
+            }
+            else
+            {
+                direction *= -1;
+            }
+        }
+        /*
         if (secondsShoot >= secondsToShoot && secondsToShoot > 1)
         {
             shoot();
             timeToShoot = 0;
-        }
+        }*/
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -150,6 +167,7 @@ public class Slime_Controller : MonoBehaviour, IDamageable, IMovable
         UnityEngine.Gizmos.DrawSphere(overCircle, 0.5f);
     }
 
+    /*
     void shoot()
     {
         // Create the Bullet from the Bullet Prefab
@@ -160,11 +178,13 @@ public class Slime_Controller : MonoBehaviour, IDamageable, IMovable
         shoot.GetComponent<Rigidbody>().velocity = shoot.transform.forward * shootForce;
         Destroy(shoot, 2.0f);
     }
-
+    */
     public void DoDamage(int amount)
     {
+        Debug.Log("Entro en Daño, " + amount);
         if (amount > 0)
         {
+            this.gameObject.SetActive(false);
         }
     }
 
